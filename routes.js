8ca8e2439ebf8cf794fe15efc0faef9cd4179fef
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-11-12 18:29:28 +0100

    add vue

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
-'use strict'
-const simple = require('./handlers/simple')
-const configured = require('./handlers/configured')
+"use strict";
+const api = require("./handlers/api");
+const configured = require("./handlers/configured");
+const staticVuePath = __dirname + "\\views\\dist";
+const express = require("express");
 
-module.exports = function (app, opts) {
+module.exports = function(app, opts) {
   // Setup routes, middleware, and handlers
-  app.get('/', simple)
-  app.get('/configured', configured(opts))
-}
+
+  app.get("/configured", configured(opts));
+  app.get("/api", api);
+  app.get("*", express.static(staticVuePath));
+};
