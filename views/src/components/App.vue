e9a50d056e728aff143a2777c3866c2cd008e99b
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-11-13 17:52:31 +0100

    add bootstrap

--- views/src/components/App.vue
+++ views/src/components/App.vue
@@ -15,6 +15,12 @@
         Checkout <code>./README.md</code> for more usages.
       </span>
     </div> -->
+    <div>
+  <b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+</div>
   </div>
 </template>
 
--- views/src/index.js
+++ views/src/index.js
@@ -1,9 +1,16 @@
-import Vue from 'vue'
-import App from './components/App.vue'
+import Vue from "vue";
+import App from "./components/App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
